706416f04db2c4cf7b672ff1edec91a0f0ee88a6
Paul Burns <paulburns115@googlemail.com>
2020-04-07 14:49:14 +0100

    included datetime

--- workspace/Real_Fake_News_project/realfakenews/models.py
+++ workspace/Real_Fake_News_project/realfakenews/models.py
@@ -1,8 +1,15 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 
 class Story(models.Model):
     story_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     story_body_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.story_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
