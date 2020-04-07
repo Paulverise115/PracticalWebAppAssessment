b23b88de576d6a5733cd0d5685406b07f46e1b9d
Paul Burns <paulburns115@googlemail.com>
2020-04-07 16:59:45 +0100

    updated paths

--- workspace/Real_Fake_News_project/Real_Fake_News_project/urls.py
+++ workspace/Real_Fake_News_project/Real_Fake_News_project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('realfakenews/', include('realfakenews.urls')),
+    path('', include('realfakenews.urls')),
     path('admin/', admin.site.urls),
 ]
