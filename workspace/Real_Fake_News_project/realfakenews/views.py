8cfa2c74c7b96276c30bb19d0cc56f036ccd38cf
Paul Burns <paulburns115@googlemail.com>
2020-04-07 15:22:08 +0100

    added views

--- workspace/Real_Fake_News_project/realfakenews/views.py
+++ workspace/Real_Fake_News_project/realfakenews/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Story
+from django.shortcuts import render
+from django.http import Http404
+
 
 def index(request):
-    return HttpResponse("This is the news page.")
+    latest_story_list = Story.objects.order_by('-pub_date')[:5]
+    context = {'latest_story_list': latest_story_list}
+    return render(request, 'realfakenews/index.html', context)
+
+
+def detail(request, story_id):
+    try:
+        story = Story.objects.get(pk=story_id)
+    except Story.DoesNotExist:
+        raise Http404("Story does not exist")
+    return render(request, 'polls/detail.html', {'story': story})
 
